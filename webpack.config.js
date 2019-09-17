4bc53c2f0ecb1ae88d6b90d8f12358018faeed03
Weirong Xu <weirongxu.raidou@gmail.com>
2019-09-17 11:40:55 +0800

    build(webpack): add webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  target: 'node',
+  resolve: {
+    mainFields: ['main', 'module'],
+    extensions: ['.js', '.ts'],
+  },
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  externals: {
+    'coc.nvim': 'commonjs coc.nvim',
+    trash: 'commonjs trash',
+    open: 'commonjs open',
+  },
+};
