bbe95ef80b2f517c070acac244dc778ae46a4262
Weirong Xu <weirongxu.raidou@gmail.com>
2020-02-20 18:52:06 +0800

    feat: Hide size label when node is directory

--- src/source/sources/file/child-columns/size.ts
+++ src/source/sources/file/child-columns/size.ts
@@ -3,6 +3,7 @@ import { fileColumnRegistrar } from '../file-column-registrar';
 import { fileHighlights } from '../file-source';
 
 fileColumnRegistrar.registerColumn('child', 'size', () => ({
+  labelVisible: (node) => !node.directory,
   draw(row, node) {
     if (node.lstat) {
       row.add(prettyBytes(node.lstat.size), { hl: fileHighlights.size });
