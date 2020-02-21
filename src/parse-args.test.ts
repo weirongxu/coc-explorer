636e3fdbe44ce275db5aaa2b01e89b41f4eef345
Weirong Xu <weirongxu.raidou@gmail.com>
2020-02-21 20:04:55 +0800

    test: Fix parse-args.test.ts

--- src/parse-args.test.ts
+++ src/parse-args.test.ts
@@ -8,16 +8,16 @@ it('should parse args', async () => {
   const rootUri = '/root/path';
   let args: Args;
   args = await Args.parse([rootUri, '--reveal', '/reveal/path', '/cwd/path']);
-  expect(await args.value(argOptions.rootUri)).toEqual('/cwd/path');
+  expect(await args.value(argOptions.rootUri)).toEqual(rootUri);
   expect(await args.value(argOptions.reveal)).toEqual('/reveal/path');
 
   args = await Args.parse([rootUri, '--toggle']);
   expect(await args.value(argOptions.toggle)).toEqual(true);
-  expect(await args.value(argOptions.rootUri)).toEqual('/root/path');
+  expect(await args.value(argOptions.rootUri)).toEqual(rootUri);
 
   args = await Args.parse([rootUri, '--no-toggle']);
   expect(await args.value(argOptions.toggle)).toEqual(false);
-  expect(await args.value(argOptions.rootUri)).toEqual('/root/path');
+  expect(await args.value(argOptions.rootUri)).toEqual(rootUri);
 
   args = await Args.parse([rootUri, '--file-child-template', '[git][fileame] [fullpath]']);
   expect(await args.value(argOptions.fileChildTemplate)).toEqual('[git][fileame] [fullpath]');
