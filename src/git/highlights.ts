d2abb325717216ab765abcb7dc557d53ba37c7b2
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2020-10-30 16:07:24 -0400

    Make linter happy

--- src/git/highlights.ts
+++ src/git/highlights.ts
@@ -3,13 +3,13 @@ import { hlGroupManager } from '../source/highlightManager';
 const hl = hlGroupManager.linkGroup.bind(hlGroupManager);
 const h = hlGroupManager.group.bind(hlGroupManager);
 
-const gitChangedPath = h('GitPathChange', `ctermfg=green guifg=green`);
+const gitChangedPath = h('GitPathChange', 'ctermfg=green guifg=green');
 
-const gitContentChange = h('GitContentChange', `ctermfg=yellow guifg=yellow`);
+const gitContentChange = h('GitContentChange', 'ctermfg=yellow guifg=yellow');
 
-const gitDeleted = h('GitDeleted', `ctermfg=red guifg=red`);
+const gitDeleted = h('GitDeleted', 'ctermfg=red guifg=red');
 
-const gitIgnored = h('GitIgnored', `ctermfg=gray guifg=gray`);
+const gitIgnored = h('GitIgnored', 'ctermfg=gray guifg=gray');
 
 export const gitHighlights = {
   gitRenamed: hl('GitRenamed', gitChangedPath.group),
