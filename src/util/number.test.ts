b373637639e7b120dede95fc2d12385038a22524
Weirong Xu <weirongxu.raidou@gmail.com>
2020-11-11 11:44:44 +0800

    test: Added toSubscriptNumbers test

--- src/util/number.test.ts
+++ src/util/number.test.ts
@@ -1,8 +1,13 @@
-import { unsigned } from './number';
+import { toSubscriptNumbers, unsigned } from './number';
 
 test('unsigned', () => {
   expect(unsigned(-1)).toEqual(0);
   expect(unsigned(0)).toEqual(0);
   expect(unsigned(1)).toEqual(1);
   expect(unsigned(1.8)).toEqual(1);
+});
+
+test('toSubscriptNumbers', () => {
+  expect(toSubscriptNumbers('✗1234')).toEqual('✗₁₂₃₄');
+  expect(toSubscriptNumbers(0.1234)).toEqual('₀.₁₂₃₄');
 });
